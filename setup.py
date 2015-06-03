88744baa97d6b4d3845dc779ec96399fb133e437
Quentin Long <qlo@yelp.com>
2015-06-02 17:08:05 -0700

    Bumped version to 0.0.39

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 base_dir = os.path.dirname(__file__)
 setup(
     name='elastalert',
-    version='0.0.38',
+    version='0.0.39',
     description='Runs custom filters on Elasticsearch and alerts on matches',
     author='Quentin Long',
     author_email='qlo@yelp.com',
