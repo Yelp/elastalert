769ca2d59bb5b91f9d89d308ec0938b6468cba7b
Quentin Long <qlo@yelp.com>
2015-06-25 10:10:08 -0700

    Fixed elastalert-test-rule entrypoint to work

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license='Copyright 2014 Yelp',
     entry_points={
         'console_scripts': ['elastalert-create-index=elastalert.create_index:main',
-                            'elastalert-test-rule=elastalert.test_rule:run_rule_test',
+                            'elastalert-test-rule=elastalert.test_rule:main',
                             'elastalert-rule-from-kibana=elastalert.rule_from_kibana:main']},
     packages=find_packages(),
     package_data={'elastalert': ['schema.yaml']},
