7b8c894ad7d2c1d21dd2176d03620fb9721b290a
Lingxiao Xia <lingxiao.xia@dragonlaw.com.hk>
2015-07-25 14:50:25 +0800

    still doesn't work this is kinda frustrating no?

--- elastalert/customrules.py
+++ elastalert/customrules.py
@@ -6,7 +6,7 @@ class CardinalityRule(RuleType):
     required_options = frozenset(['cardinality_term','max_cardinality', 'timeframe'])
 
     def __init__(self, *args):
-        super(FrequencyRule, self).__init__(*args)
+        super(CardinalityRule, self).__init__(*args)
         self.ts_field = self.rules.get('timestamp_field', '@timestamp')
         self.get_ts = lambda event: event[0][self.ts_field]
 
@@ -24,22 +24,28 @@ class CardinalityRule(RuleType):
                 key = 'all'
 
             # Store the timestamps of recent occurrences, per key
-            self.occurrences.setdefault(key, EventWindow(self.rules['cardinality_term'],self.rules['timeframe'], getTimestamp=self.get_ts)).append((event, 1))
+            self.occurrences.setdefault(key, CardinalityWindow(self.rules['cardinality_term'],self.rules['timeframe'], getTimestamp=self.get_ts)).append((event, 1))
             self.check_for_match(key)
 
     def check_for_match(self, key):
         # Match if, after removing old events, we hit max_cardinality
         if self.occurrences[key].count() >= self.rules['max_cardinality']:
-            event = self.occurrences[key].data[-1][0]
-            self.add_match(event)
-            self.occurrences.pop(key)
+            if self.rules['query_key']:
+                match = {self.rules['query_key']:key, 'events':{}}}
+            else: 
+                match = {'events':{}}
+            for item in self.occurrences[key].data:
+                match['events'][item[0][self.rules['cardinality_term']]]=self.get_ts(item)
+            self.add_match(match)
+            self.occurrences[key].alerted()
 
     def garbage_collect(self, timestamp):
         """ Remove all occurrence data that is beyond the timeframe away """
         stale_keys = []
         for key, window in self.occurrences.iteritems():
-            if timestamp - window.data[-1][0][self.ts_field] > self.rules['timeframe']:
-                stale_keys.append(key)
+            if window.data:
+                if timestamp - window.data[-1][0][self.ts_field] > self.rules['timeframe']:
+                    stale_keys.append(key)
         map(self.occurrences.pop, stale_keys)
 
     def get_match_str(self, match):
@@ -55,14 +61,17 @@ class CardinalityWindow(object):
     """ A container for hold event counts for rules which need a chronological ordered event window. """
 
     def __init__(self, cardinality_term, timeframe, onRemoved=None, getTimestamp=lambda e: e[0]['@timestamp']):
-    	self.cterm = cardinality_term
+        self.cterm = cardinality_term
         self.timeframe = timeframe
         self.onRemoved = onRemoved
         self.get_ts = getTimestamp
         self.data = deque()
+        self.logBook = {}
         self.running_count = 0
 
-    def clear(self):
+    def alerted(self, timestamp):
+        for event in self.data:
+            self.logBook[event[0][self.cterm]]['last_alert'] = self.get_ts(self.data[-1])
         self.data = deque()
         self.running_count = 0
 
@@ -72,27 +81,38 @@ class CardinalityWindow(object):
         window size is less than timeframe. """
         # If the event occurred before our 'latest' event
         if self.cterm in event[0].keys():
-        	ele_to_remove = []
-        	for ele in self.data:
-        		if event[0][self.cterm]==ele[0][self.cterm]:
-        			if self.get_ts(event)>self.get_ts(ele):
-        				ele_to_remove.append(ele)
-        			else:
-        				event = None
-        	for item in ele_to_remove:
-        		self.data.remove(item)
-        		self.running_count -= item[1]
-        	if event:
-		        if len(self.data) and self.get_ts(self.data[-1]) > self.get_ts(event):
-		            self.append_middle(event)
-		        else:
-		            self.data.append(event)
-		            self.running_count += event[1]
-
+            cterm = event[0][self.cterm]
+            if cterm in self.logBook.keys():
+                if self.get_ts(event) > self.logBook[cterm]['lastest']:
+                    self.logBook[cterm]['latest']=self.get_ts(event)
+                    if not self.logBook[cterm]['last_alert']:
+                        self.appendData(event)
+                    elif self.get_ts(event)-self.logBook['last_alert']>=self.timeframe:
+                        self.logBook['last_alert'] = None
+                        self.appendData(event)
+            else:
+                self.logBook[cterm]={'latest':self.get_ts(event),'last_alert':None}
+                self.appendData(event)
+            
+    def appendData(self,event):
+        self.removeData(event[0][self.cterm])
+        if len(self.data) and self.get_ts(self.data[-1]) > self.get_ts(event):
+            self.append_middle(event)
+        else:
+            self.data.append(event)
+            self.running_count += event[1]
         while self.duration() >= self.timeframe:
             oldest = self.data.popleft()
             self.running_count -= oldest[1]
             self.onRemoved and self.onRemoved(oldest)
+
+    def removeData(cterm):
+        dataToRemove = []
+        for ele in self.data:
+            if ele[0][self.cterm] == cterm:
+                dataToRemove.append(ele)
+        for item in dataToRemove:
+            self.data.remove(item)
 
     def duration(self):
         """ Get the size in timedelta of the window. """
