4b2ccd000e0e764eb1026b793b49d9bf765ca5a3
Quentin Long <qlo@yelp.com>
2016-05-02 11:48:16 -0700

    Small bug fixes

--- elastalert/alerts.py
+++ elastalert/alerts.py
@@ -110,7 +110,7 @@ class JiraFormattedMatchString(BasicMatchString):
     def _add_match_items(self):
         match_items = dict([(x, y) for x, y in self.match.items() if not x.startswith('top_events_')])
         json_blob = self._pretty_print_as_json(match_items)
-        preformatted_text = '{{code:json}}{0}{{code}}'.format(json_blob)
+        preformatted_text = u'{{code:json}}{0}{{code}}'.format(json_blob)
         self.text += preformatted_text
 
 
