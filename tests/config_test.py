d438153ea13a30bd75266567306d88def4572f6b
mshaylor <mshaylor@mercuria.com>
2017-03-09 13:59:06 +0000

    Add support for import feature to rules

--- tests/config_test.py
+++ tests/config_test.py
@@ -69,6 +69,28 @@ def test_import_rules():
         assert mock_import.call_args_list[0][0][3] == ['Alerter']
 
 
+def test_import_import():
+    import_rule = copy.deepcopy(test_rule)
+    del(import_rule['es_host'])
+    del(import_rule['es_port'])
+    import_rule['import'] = 'importme.yaml'
+    import_me = {
+        'es_host': 'imported_host',
+        'es_port': 12349,
+        'email': 'ignored@email',  # overwritten by the email in import_rule
+    }
+
+    with mock.patch('elastalert.config.yaml_loader') as mock_open:
+        mock_open.side_effect = [import_rule, import_me]
+        rules = load_configuration('blah.yaml', test_config)
+        assert mock_open.call_args_list[0][0] == ('blah.yaml',)
+        assert mock_open.call_args_list[1][0] == ('importme.yaml',)
+        assert len(mock_open.call_args_list) == 2
+        assert rules['es_port'] == 12349
+        assert rules['es_host'] == 'imported_host'
+        assert rules['email'] == ['test@test.test']
+
+
 def test_load_inline_alert_rule():
     test_rule_copy = copy.deepcopy(test_rule)
     test_rule_copy['alert'] = [
