73d09befb5d93f4fd217b77be8e217b2091ec72a
idealphase <mynameisphase@gmail.com>
2018-04-02 04:21:31 +0700

    Added LineNotify Alerter

--- elastalert/alerts.py
+++ elastalert/alerts.py
@@ -1560,3 +1560,30 @@ class StrideAlerter(Alerter):
         return {'type': 'stride',
                 'stride_cloud_id': self.stride_cloud_id,
                 'stride_converstation_id': self.stride_converstation_id}
+class LineNotifyAlerter(Alerter):
+    """ Created a Line Notify for each alert """ 
+    required_option = frozenset(["linenotify_access_token"])
+
+    def __init__(self, rule):
+        super(LineNotifyAlerter, self).__init__(rule)
+        self.linenotify_access_token = self.rule["linenotify_access_token"]
+
+    def alert(self, matches):
+        body = self.create_alert_body(matches)
+        # post to Line Notify
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer {}".format(self.linenotify_access_token)
+        }
+        payload = {
+            "message":body        
+        }
+        try:
+            response = requests.post("https://notify-api.line.me/api/notify", data=payload, headers=headers)
+            response.raise_for_status()
+        except RequestException as e:
+            raise EAException("Error posting to Line Notify: %s" % e)
+        elastalert_logger.info("Alert sent to Line Notify")
+
+    def get_info(self):
+        return {"type": "linenotify","linenotify_access_token": self.linenotify_access_token}
