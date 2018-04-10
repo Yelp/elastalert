7504a5a10a68700f2366e1133a726560d124aa45
Peter Scopes <peter.scopes@nccgroup.trust>
2018-04-10 15:24:26 +0100

    Updated test_rule to be able to handle es version >= 5

--- elastalert/test_rule.py
+++ elastalert/test_rule.py
@@ -54,7 +54,7 @@ class MockElastAlerter(object):
         es_client = elasticsearch_client(conf)
 
         try:
-            is_five = es_client.info()['version']['number'].startswith('5')
+            ElastAlerter.modify_rule_for_ES5(conf)
         except Exception as e:
             print("Error connecting to ElasticSearch:", file=sys.stderr)
             print(repr(e)[:2048], file=sys.stderr)
@@ -62,13 +62,16 @@ class MockElastAlerter(object):
                 exit(1)
             return None
 
-        if is_five:
-            ElastAlerter.modify_rule_for_ES5(conf)
-
         start_time = ts_now() - datetime.timedelta(days=args.days)
         end_time = ts_now()
         ts = conf.get('timestamp_field', '@timestamp')
-        query = ElastAlerter.get_query(conf['filter'], starttime=start_time, endtime=end_time, timestamp_field=ts, five=is_five)
+        query = ElastAlerter.get_query(
+            conf['filter'],
+            starttime=start_time,
+            endtime=end_time,
+            timestamp_field=ts,
+            five=conf['five']
+        )
         index = ElastAlerter.get_index(conf, start_time, end_time)
 
         # Get one document for schema
@@ -94,7 +97,7 @@ class MockElastAlerter(object):
             endtime=end_time,
             timestamp_field=ts,
             sort=False,
-            five=is_five
+            five=conf['five']
         )
         try:
             res = es_client.count(index, doc_type=doc_type, body=count_query, ignore_unavailable=True)
