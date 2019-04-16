dc8f39f2b4e26bd50856889dc057e013023654c9
Abhishek Jaisingh <abhi2254015@gmail.com>
2019-04-17 03:10:10 +0530

    Add Prometheus Metrics
    Start exporting metrics in Prometheus format on port 9090.
    Relevant Metrics added, most of the metrics have a *rule_name*_label:
    (All metrics except 6th are per rule)
    1. Number of hits
    2. Number of matches
    3. Time taken to evaluate rule
    4. Number of alerts sent
    5. Number of alerts not sent
    6. Total Number of elastalert_errors
    7. Number of silenced alerts

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'jsonschema>=2.6.0,<3.0.0',
         'mock>=2.0.0',
         'PyStaticConfiguration>=0.10.3',
+        'prometheus_client>=0.6.0',
         'python-dateutil>=2.6.0,<2.7.0',
         'PyYAML>=3.12',
         'requests>=2.10.0',
