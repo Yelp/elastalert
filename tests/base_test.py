a965e762f84e1210da741b2b4a6c83ec3bcfd4bc
Jeff Ashton <Jeff.Ashton@D2L.com>
2019-06-24 14:59:23 -0400

    Trying to fix formatting

--- tests/base_test.py
+++ tests/base_test.py
@@ -22,8 +22,8 @@ from elastalert.util import ts_now
 from elastalert.util import ts_to_dt
 from elastalert.util import unix_to_dt
 
-START_TIMESTAMP = '2014-09-26T12:34:45Z'
-END_TIMESTAMP = '2014-09-27T12:34:45Z'
+START_TIMESTAMP = '2014-09-26T12:34:45.000000Z'
+END_TIMESTAMP = '2014-09-27T12:34:45.000000Z'
 START = ts_to_dt(START_TIMESTAMP)
 END = ts_to_dt(END_TIMESTAMP)
 
