507ac63ba0ca07e9d0d6da9c5ed75b4ab6bb6db3
Raghu Chinnannan <40737704+raghuchinnannan@users.noreply.github.com>
2019-07-15 18:49:30 +0530

    Added Squadcast Alerter class
    Added Squadcast Alerter class to send alerts to Squadcast

--- elastalert/alerts.py
+++ elastalert/alerts.py
@@ -1486,6 +1486,42 @@ class PagerTreeAlerter(Alerter):
     def get_info(self):
         return {'type': 'pagertree',
                 'pagertree_integration_url': self.url}
+    
+    
+    
+class SquadCastAlerter(Alerter):
+    """ Creates a Squadcast Incident for each alert """
+    required_options = frozenset(['squadcast_integration_url'])
+
+    def __init__(self, rule):
+        super(SquadCastAlerter, self).__init__(rule)
+        self.url = self.rule['squadcast_integration_url']
+        self.squadcast_proxy = self.rule.get('squadcast_proxy', None)
+
+    def alert(self, matches):
+        # post to squadcast
+        headers = {'content-type': 'application/json'}
+        # set https proxy, if it was provided
+        proxies = {'https': self.squadcast_proxy} if self.squadcast_proxy else None
+        payload = {
+            "alert_source": "ElastAlert",
+            "event_type": "create",
+            "Id": str(uuid.uuid4()),
+            "Title": self.create_title(matches),
+            "Description": self.create_alert_body(matches)
+        }
+
+        try:
+            response = requests.post(self.url, data=json.dumps(payload, cls=DateTimeEncoder), headers=headers, proxies=proxies)
+            response.raise_for_status()
+        except RequestException as e:
+            raise EAException("Error posting to Squadcast: %s" % e)
+        elastalert_logger.info("Trigger sent to Squadcast")
+
+    def get_info(self):
+        return {'type': 'squadcast',
+                'squadcast_integration_url': self.url}
+
 
 
 class ExotelAlerter(Alerter):
