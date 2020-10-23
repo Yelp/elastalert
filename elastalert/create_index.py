02e5278302dba3abcd402dbe5c30ce8e88929d7c
Naoyuki Sano <nsano@ae.em-net.ne.jp>
2020-10-24 07:33:12 +0900

    Update create_index.py

--- elastalert/create_index.py
+++ elastalert/create_index.py
@@ -192,7 +192,7 @@ def main():
 
     if filename:
         with open(filename) as config_file:
-            data = yaml.load(config_file, Loader=yaml.SafeLoader)
+            data = yaml.load(config_file, Loader=yaml.UnsafeLoader)
         host = args.host if args.host else data.get('es_host')
         port = args.port if args.port else data.get('es_port')
         username = args.username if args.username else data.get('es_username')
