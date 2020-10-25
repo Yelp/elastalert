293290d76d3a58a5a93a445d886ba6309c624360
nsano-rururu <nsano@ae.em-net.ne.jp>
2020-10-26 00:51:31 +0900

    Revert the version of configparser

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'aws-requests-auth>=0.3.0',
         'blist>=1.3.6',
         'boto3>=1.4.4',
-        'configparser>=5.0.1',
+        'configparser>=3.5.0',
         'croniter>=0.3.16',
         'elasticsearch==7.0.0',
         'envparse>=0.2.0',
