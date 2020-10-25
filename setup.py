77ba84d15604c0bcb56f3a7ceef5fb0c65640c51
nsano-rururu <nsano@ae.em-net.ne.jp>
2020-10-26 01:12:44 +0900

    remove cffi

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'requests>=2.10.0',
         'stomp.py>=4.1.17',
         'texttable>=0.8.8',
-        'twilio>=6.0.0,<6.1',
-        'cffi>=1.11.5'
+        'twilio>=6.0.0,<6.1'
     ]
 )
