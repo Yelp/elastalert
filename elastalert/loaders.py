62efda09cc2794c3f9e54aed2d323ccd7891f69f
nsano-rururu <nsano@ae.em-net.ne.jp>
2021-01-15 00:41:05 +0900

    fix zabbix

--- elastalert/loaders.py
+++ elastalert/loaders.py
@@ -25,6 +25,7 @@ from .util import ts_to_dt
 from .util import ts_to_dt_with_format
 from .util import unix_to_dt
 from .util import unixms_to_dt
+from .zabbix import ZabbixAlerter
 
 
 class RulesLoader(object):
@@ -77,7 +78,8 @@ class RulesLoader(object):
         'post': alerts.HTTPPostAlerter,
         'pagertree': alerts.PagerTreeAlerter,
         'linenotify': alerts.LineNotifyAlerter,
-        'hivealerter': alerts.HiveAlerter
+        'hivealerter': alerts.HiveAlerter,
+        'zabbix': ZabbixAlerter
     }
 
     # A partial ordering of alert types. Relative order will be preserved in the resulting alerts list
