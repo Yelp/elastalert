d3fd9bb3e9f4624822e73f1e7306c648ec4da2c8
Feroz Salam <feroz@argh.in>
2021-03-02 09:19:13 +0000

    Migrate the FAQ to Read the Docs
    - Also add a short README on building the documentation
    - Make some tweaks to allow the generation of documentation from Markdown rather than ReStructured Text

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,13 +19,13 @@ import sphinx_rtd_theme
 # -- General configuration -----------------------------------------------------
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ["m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 # source_encoding = 'utf-8'
