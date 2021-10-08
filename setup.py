9c9385099c86d27b6dca0ebbd97a63a3eb6711b3
Jason Ertel <jason.ertel@securityonionsolutions.com>
2021-10-08 07:58:41 -0400

    Upgrade to Python 3.10; Upgrade Sphinx to 4.2.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Discussion Forum": "https://github.com/jertel/elastalert2/discussions",
     },
     classifiers=[
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
